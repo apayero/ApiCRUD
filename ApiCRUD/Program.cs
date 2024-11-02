810888a30b8eda660af8b7fec02a80fda14d14ff
apayero <atpayero@gmail.com>
2024-11-02 10:26:25 -0400

    Alex ahora si, podemos trabajar juntos remotamente.

--- ApiCRUD/Program.cs
+++ ApiCRUD/Program.cs
@@ -2,7 +2,7 @@ using ApiCRUD;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-// Inicio de área de los servicios: Alex Mota
+// Inicio de área de los servicios: Alex Mota y Payero pueden actualizar código juntos
 
 builder.Services.AddDbContext<ApplicationDbContext>(opciones =>
 opciones.UseSqlServer("name=DefaultConnection"));
